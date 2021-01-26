439ca331b3dc85e53c865756e2f7c88830c4d28a
ValeriiaSedova <valeriiasedova@gmail.com>
2021-01-26 23:42:00 +0100

    Finished Model (hopefully)

--- src_to_implement/model.py
+++ src_to_implement/model.py
@@ -18,18 +18,34 @@ class Model(torch.nn.Module):
             torch.nn.Flatten(),
             torch.nn.Linear(2),
             torch.nn.Sigmoid()]
+
+    def forward(self, input_tensor):
+        x = input_tensor
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+
  
 class ResBlock:
 
     def __init__(self, output_channels, stride):
 
         self.layers =[
-            torch.nn.Conv2d(64, 3, stride),
+            torch.nn.Conv2d(output_channels, 3, stride),
             torch.nn.BatchNorm(),
             torch.nn.ReLU(),
             torch.nn.Conv2d(output_channels, 1),
             torch.nn.BatchNorm(),
             torch.nn.ReLU()]
+
+
+    def forward(self, input_tensor):
+        x = input_tensor
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+    
         
 
 
